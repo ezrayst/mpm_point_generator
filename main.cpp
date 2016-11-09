01268a74b5870aed89f12c7d6aadddfe105ec7d0
ezrayst <ezrayst@berkeley.edu>
2016-11-08 22:54:14 -0800

    add main file that will run the point generator

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// The main code to generate points within a rectangle 
+//
+// Done by Ezra Y. S. Tjung <ezrayst@berkeley.edu>
+// 
+// University of California, Berkeley
+//
+
+#include <iostream>
+
+using namespace std;
