a15ef156ac21dfbd5a237d225245a8dd101756d9
ezrayst <ezrayst@berkeley.edu>
2017-01-22 23:24:26 -0800

    make x y grid different

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
-// The main code to generate points within a rectangle
-//
-// Done by Ezra Y. S. Tjung <ezrayst@berkeley.edu>
-//
-// University of California, Berkeley
-//
-
 // MPM Point Generator
+//
+// The main code to generate points within a rectangle
 // Rectangular with input: (x1,y1) coordinates left bottom corner
 //                         (x2,y2) coordinates right top corner
-//                         (n)     making n-by-n grid
+//                         (m)     making n grid in x-direction
+//						   (n)     making m grid in y-direction
 //
-
+//
+// Input the left bottom and right top coordinates
+// Users will need to type the coordinate
+// Test: (x1,y1,x2,y2) = (0,0,1,1)
+//                 (m) = (3)
+//                 (n) = (3)
+// Generate points
+// points: [Node#   x-coor   y-coor]
+// The points will be saved to a .txt file
 
 #include <iostream>
 #include <fstream>
@@ -19,21 +23,11 @@ using namespace std;
 
 int main()
 {
-	double x1;
-	double y1;
-	double x2;
-	double y2;
-	int n;
+	double x1, y1;
+	double x2, y2;
+	int i, j, n, m, p;
 	double hx;          // the distance of every point in x-direction
 	double hy;          // the distance of every point in y-direction
-
-	int i;
-	int j;
-
-	// Input the left bottom and right top coordinates
-	// Users will need to type the coordinate
-	// Test: (x1,y1,x2,y2) = (0,0,1,1)
-	//                 (n) = (3)
 
 	cout << "Enter left bottom corner coordinate (x1): \n";
 	cin >> x1;
@@ -47,33 +41,33 @@ int main()
 	cout << "Enter right top corner coordinate (y2): \n";
 	cin >> y2;
 
-	cout << "Enter the number of grid points on an edge (n): \n";
+	cout << "Enter the number of grid points in x direction (m): \n";
+	cin >> m;
+
+	cout << "Enter the number of grid points in y direction (n): \n";
 	cin >> n;
 
-	// Get hx and hy
-	hx = (x2 - x1) / (n - 1);
+	hx = (x2 - x1) / (m - 1);
 	hy = (y2 - y1) / (n - 1);
 
-
-	// Generate points
-	// Naming goes from 0 to n^2, started at bottom left to the right and then up
-	// points: [Node#   x-coor   y-coor]
-	// The points will be saved to a .txt file
-
-	std::ofstream filename("mpm_points.txt");
-
-	double points[n*n][3];          // the distance of every point in y-direction
-
+	double points[m*n][3];          // the distance of every point in y-direction
 	for (i=0; i < n; ++i)			// i is respect to y
-		for (j=0; j < n; ++j)		// j is respect to x
+		for (j=0; j < m; ++j)		// j is respect to x
 		{
-			points[i*n + j][1] = i*n + j + 1;
-			points[i*n + j][2] = x1 + j*hx;
-			points[i*n + j][3] = y1 + i*hy;
-
-			filename << points[i*n + j][1] << " " << points[i*n + j][2] << " " << points[i*n + j][3] << "\n";
+			p = i*n + j;
+			points[p][1] = x1 + j*hx;
+			points[p][2] = y1 + i*hy;
 		}
 
-	return 0;
+	std::ofstream filename("mpm_points.txt");
+	for (i=0; i < n; ++i)			// i is respect to y
+		for (j=0; j < m; ++j)		// j is respect to x
+		{
+			p = i*n + j;
+			filename << points[p][1] << "," << points[p][2] << "\n";
+		}
+	filename.close();
+	cout << "The file has been generated.";
 }
+
 
