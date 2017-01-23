77fcd0335d98a458507cf47547c732686c6b7cae
ezrayst <ezrayst@berkeley.edu>
2017-01-23 12:15:31 -0800

    change variable names

--- main.cpp
+++ main.cpp
@@ -18,62 +18,58 @@
 
 #include <iostream>
 #include <fstream>
-#include <array>
 #include <vector>
-
-using namespace std;
 
 int main()
 {
 	double x1, y1;
 	double x2, y2;
-	int i, j, k, n, m, p;
-	double hx;          // the distance of every point in x-direction
-	double hy;          // the distance of every point in y-direction
+	int i, j, k, num_points_x, num_points_y, num_points;
+	double x_spacing, y_spacing;
 
-	cout << "Enter left bottom corner coordinate (x1): \n";
-	cin >> x1;
+	std::cout << "Enter left bottom corner coordinate (x1): \n";
+	std::cin >> x1;
 
-	cout << "Enter left bottom corner coordinate (y1): \n";
-	cin >> y1;
+	std::cout << "Enter left bottom corner coordinate (y1): \n";
+	std::cin >> y1;
 
-	cout << "Enter right top corner coordinate (x2): \n";
-	cin >> x2;
+	std::cout << "Enter right top corner coordinate (x2): \n";
+	std::cin >> x2;
 
-	cout << "Enter right top corner coordinate (y2): \n";
-	cin >> y2;
+	std::cout << "Enter right top corner coordinate (y2): \n";
+	std::cin >> y2;
 
-	cout << "Enter the number of grid points in x direction (m): \n";
-	cin >> m;
+	std::cout << "Enter the number of grid points in x direction (m): \n";
+	std::cin >> num_points_x;
 
-	cout << "Enter the number of grid points in y direction (n): \n";
-	cin >> n;
+	std::cout << "Enter the number of grid points in y direction (n): \n";
+	std::cin >> num_points_y;
 
-	hx = (x2 - x1) / (m - 1);
-	hy = (y2 - y1) / (n - 1);
-	p = m*n;
+	x_spacing = (x2 - x1) / (num_points_x - 1);
+	y_spacing = (y2 - y1) / (num_points_y - 1);
+	num_points = num_points_x*num_points_y;
 
-	std::vector<double> xarr(p);
-	std::vector<double> yarr(p);
+	std::vector<double> xarr(num_points);
+	std::vector<double> yarr(num_points);
 
-	for (i=0; i < n; ++i)			// i is respect to y
-		for (j=0; j < m; ++j)		// j is respect to x
+	for (i=0; i < num_points_y; ++i)			// i is respect to y
+		for (j=0; j < num_points_x; ++j)		// j is respect to x
 		{
-			k = i*n + j;
-			xarr[k] = x1 + j*hx;
-			yarr[k] = y1 + i*hy;
+			k = i*num_points_y + j;
+			xarr[k] = x1 + j*x_spacing;
+			yarr[k] = y1 + i*y_spacing;
 		}
 
 	std::ofstream mpm_points("mpm_points.txt");
-	for (i=0; i < n; ++i)			// i is respect to y
-		for (j=0; j < m; ++j)		// j is respect to x
+	for (i=0; i < num_points_y; ++i)			// i is respect to y
+		for (j=0; j < num_points_x; ++j)		// j is respect to x
 		{
-			k = i*n + j;
+			k = i*num_points_y + j;
 			mpm_points << xarr[k] << "," << yarr[k] << "\n";
 		}
 	mpm_points.close();
 
-	cout << "The file has been generated.";
+	std::cout << "The file has been generated.";
 }
 
 
