d490935bf5c61d7a54104f2c5925497dc7c3ae03
ezrayst <ezrayst@berkeley.edu>
2017-01-30 11:26:28 -0800

    shared pointer

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 //                         (x2) x-coordinate right top corner
 //                         (y2) y-coordinate right top corner
 //                         (m)     making n grid in x-direction
-//						   (n)     making m grid in y-direction
+//                         (n)     making m grid in y-direction
 //
 
 #include <iostream>
@@ -50,28 +50,34 @@ int main()
 	}
 
 	inputFile >> x1 >> y1 >> x2 >> y2 >> x_spacing >> y_spacing;
-
+	inputFile.close();
+	
 	std::cout << "The input file has been read." << "\n";
 
 	const int num_points_x = (x2 - x1) / x_spacing + 1;
 	const int num_points_y = (y2 - y1) / y_spacing + 1;
 
-	std::vector<mat_points> points;
+	std::vector<std::shared_ptr<mat_points>> points;
 
 	for (int i=0; i < num_points_y; ++i)			// i is respect to y
 		for (int j=0; j < num_points_x; ++j)		// j is respect to x
 		{
 			int k = i*num_points_y + j;
 			std::array<double, 2> coord{{x1 + j*x_spacing, y1 + i*y_spacing}};
-			mat_points dummy_points(k, coord);
+			std::shared_ptr<mat_points> dummy_points = std::make_shared<mat_points>(k, coord);
 			points.push_back(dummy_points);
 		}
-
 	std::ofstream outputFile("mpm_points.txt");
 	for (auto &point : points)
 	{
-		outputFile << point.get_id() << "," << point.get_coords().at(0) << "," << point.get_coords().at(1) << "\n";
+		auto p = point.get();
+		outputFile << p->get_id() << "," << p->get_coords().at(0) << "," << p->get_coords().at(1) << "\n";
+//		std::cout << "Count before = " << point.use_count() << '\n';
+		point.reset();
+//		std::cout << "Count after = " << point.use_count() << '\n';
+
 	}
+
 	outputFile.close();
 
 	std::cout << "The output file has been generated.";
