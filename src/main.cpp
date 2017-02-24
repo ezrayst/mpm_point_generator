79b04ff22e5c72ac525359eb24497d403a08970d
Ezra Tjung <ezrayst@berkeley.edu>
2017-02-24 11:42:03 -0800

    fixed Krisnha's comments part 2

--- src/main.cpp
+++ src/main.cpp
@@ -24,51 +24,48 @@ int main()
 	try
 	{
 		//! Reading input file
+		//! Error would be reported
 		std::ifstream inputFile;
+		inputFile.exceptions ( std::ifstream::failbit | std::ifstream::badbit );
 		inputFile.open("../bin/input_coords.txt");
-
-		if (inputFile.fail())
-		{
-			throw "Error: input file cannot be read.";
-		}
 
 		inputFile >> x1 >> y1 >> x2 >> y2 >> x_spacing >> y_spacing;
 		inputFile.close();
-	}
-	
-	catch (char const* errormesg)
-	{
-			std::cerr << errormesg;
-			return EXIT_FAILURE;
-	}
 
+		std::cout << "The input file has been read." << "\n";
 	
-	std::cout << "The input file has been read." << "\n";
-	
-	//! Main calculation
-	const int num_points_x = (x2 - x1) / x_spacing + 1;
-	const int num_points_y = (y2 - y1) / y_spacing + 1;
+		//! Main calculation
+		const int num_points_x = (x2 - x1) / x_spacing + 1;
+		const int num_points_y = (y2 - y1) / y_spacing + 1;
 
-	std::vector<std::shared_ptr<MaterialPoint>> points;
+		std::vector<std::shared_ptr<MaterialPoint>> points;
 
-	for (int i=0; i < num_points_y; ++i)			//!< i is respect to y
-		for (int j=0; j < num_points_x; ++j)		//!< j is respect to x
+		for (int i=0; i < num_points_y; ++i)			//!< i is respect to y
+			for (int j=0; j < num_points_x; ++j)		//!< j is respect to x
+			{
+				int k = i*num_points_y + j;
+				std::array<double, 2> coord{{x1 + j*x_spacing, y1 + i*y_spacing}};
+				std::shared_ptr<MaterialPoint> dummy_points(new MaterialPoint(k, coord));
+				points.push_back(dummy_points);
+			}
+
+		std::ofstream outputFile("../bin/mpm_points.txt");
+		for (auto& point : points)
 		{
-			int k = i*num_points_y + j;
-			std::array<double, 2> coord{{x1 + j*x_spacing, y1 + i*y_spacing}};
-			std::shared_ptr<MaterialPoint> dummy_points(new MaterialPoint(k, coord));
-			points.push_back(dummy_points);
+			outputFile << point->id() << "," << point->coords().at(0) << "," << point->coords().at(1) << "\n";
+			point.reset();
 		}
-	std::ofstream outputFile("../bin/mpm_points.txt");
-	for (auto& point : points)
-	{
-		outputFile << point->id() << "," << point->coords().at(0) << "," << point->coords().at(1) << "\n";
-		point.reset();
+
+		outputFile.close();
+
+		std::cout << "The output file has been generated.";
 	}
-
-	outputFile.close();
-
-	std::cout << "The output file has been generated.";
+	
+	catch (std::ifstream::failure e)
+	{
+		std::cerr << "Error: input file cannot be read.";
+		return EXIT_FAILURE;
+	}
 }
 
 
