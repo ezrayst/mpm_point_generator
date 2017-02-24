f8085516591601c97296831410d3bea2f26311f0
Ezra Tjung <ezrayst@berkeley.edu>
2017-02-24 12:46:35 -0800

    add comments for point.h

--- include/point.h
+++ include/point.h
@@ -12,10 +12,11 @@ public:
 
 	//! Get the private properties using two functions
 	//! To get the id_ and coords_
-	unsigned const id()
+	unsigned id() const
 	{
 		return id_;
 	}
+
 	std::array<double, 2> const coords()
 	{
 		return coords_;
@@ -23,6 +24,9 @@ public:
 
 private:
 	//! These are the parameters of the class MaterialPoint
+	//! id_ would be a positive integer starting from 0 as index
+	//! coords_ would be an array of size 2, containing x and y coordinates
+
 	unsigned id_;
 	std::array<double, 2> coords_;
 };
