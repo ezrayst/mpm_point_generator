fc6ab21769abf0765a53ff6de75611ebdc6b15ad
Ezra Tjung <ezrayst@berkeley.edu>
2017-04-04 23:47:02 -0700

    make mesh.h

--- src/main.cc
+++ src/main.cc
@@ -16,12 +16,9 @@
 #include <memory>
 #include <vector>
 
-//#include "mesh.h"
-#include "point.h"
+#include "mesh.h"
 
 int main() {
-  double x1, x2, y1, y2, x_spacing, y_spacing;
-
   try {
     //! Reading input file
     //! Error would be reported
@@ -42,41 +39,11 @@ int main() {
     if (outputFilename == "")
       outputFilename = "../bin/mpm_points.txt";
    
-    std::ifstream inputFile;
-    inputFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    inputFile.open(inputFilename);
+    
+    Mesh points;
+    points.ReadFile(inputFilename);
+    points.WriteOutputFile(outputFilename);  
 
-    inputFile >> x1 >> y1 >> x2 >> y2 >> x_spacing >> y_spacing;
-    inputFile.close();
-
-    std::cout << "The input file has been read."
-              << "\n";
-
-    //! Main calculation
-    const int num_points_x = static_cast<int>(ceil((x2 - x1) / x_spacing + 1));
-    const int num_points_y = static_cast<int>(ceil((y2 - y1) / y_spacing + 1));
-
-    std::vector<std::shared_ptr<MaterialPoint>> points;
-    int k = 0;
-
-    for (int i = 0; i < num_points_y; ++i)
-      for (int j = 0; j < num_points_x; ++j) {
-        std::array<double, 2> coord{{x1 + j * x_spacing, y1 + i * y_spacing}};
-        std::shared_ptr<MaterialPoint> point = std::make_shared<MaterialPoint>(k, coord);
-        points.emplace_back(point);
-        k += 1;
-      }
-
-    std::ofstream outputFile(outputFilename);
-    for (auto& point : points) {
-      outputFile << point->id() << "," << point->coords().at(0) << ","
-                 << point->coords().at(1) << "\n";
-    }
-
-    outputFile.close();
-
-    std::cout << "The output file has been generated."
-              << "\n";
   }
 
   catch (std::exception& except) {
