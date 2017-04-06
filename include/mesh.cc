d2b7d21eab3ebdd71d7fb989f57e3485933bdf0d
Ezra Tjung <ezrayst@berkeley.edu>
2017-04-05 23:26:34 -0700

    include mesh.cc

---
+++ include/mesh.cc
@@ -0,0 +1,41 @@
+#include "mesh.h"
+
+void Mesh::read_file(const std::string &inputfilename) {
+  double x1, x2, y1, y2, x_spacing, y_spacing;
+
+  std::ifstream inputFile;
+  inputFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  inputFile.open(inputfilename);
+
+  inputFile >> x1 >> y1 >> x2 >> y2 >> x_spacing >> y_spacing;
+  inputFile.close();
+
+  std::cout << "The input file has been read."
+            << "\n";
+
+  //! Main calculation
+  const int num_points_x = static_cast<int>(ceil((x2 - x1) / x_spacing + 1));
+  const int num_points_y = static_cast<int>(ceil((y2 - y1) / y_spacing + 1));
+
+  int k = 0;
+
+  for (int i = 0; i < num_points_y; ++i)
+    for (int j = 0; j < num_points_x; ++j) {
+      std::array<double, 2> coord{{x1 + j * x_spacing, y1 + i * y_spacing}};
+      points_.emplace_back(
+          std::unique_ptr<MaterialPoint>(new MaterialPoint(k, coord)));
+      k += 1;
+    }
+}
+
+void Mesh::write_output_file(const std::string &outputfilename) {
+  std::ofstream outputFile(outputfilename);
+  for (auto const &point : points_) {
+    outputFile << point->id() << "," << point->coords().at(0) << ","
+               << point->coords().at(1) << "\n";
+  }
+
+  outputFile.close();
+  std::cout << "The output file has been generated."
+            << "\n";
+}
