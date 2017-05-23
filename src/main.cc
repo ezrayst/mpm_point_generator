1c85a54c92b956336483ee637f7b9b11967f1c4d
Ezra Tjung <ezrayst@berkeley.edu>
2017-05-23 16:15:37 -0700

    master: stable version

--- src/main.cc
+++ src/main.cc
@@ -33,9 +33,9 @@ int main() {
     std::string outputfilename_stress;
 
     std::cout
-        << "Type the input file name, default: [../bin/soilParticles.json]: ";
+        << "Type the input file name, default: [../bin/soilParticles.txt]: ";
     std::getline(std::cin, inputfilename);
-    if (inputfilename == "") inputfilename = "../bin/soilParticles.json";
+    if (inputfilename == "") inputfilename = "../bin/soilParticles.txt";
 
     std::cout << "Type the output file name for soil particles, default: "
                  "[../bin/soilParticles.smf]: ";
